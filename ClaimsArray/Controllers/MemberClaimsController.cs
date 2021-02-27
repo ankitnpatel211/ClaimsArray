ef02cc3b8b22cb23a0a111e66743706d7db80373
Ankit Patel <wd.ankit.patel@gmail.com>
2021-02-27 15:32:36 -0600

    Updates

--- ClaimsArray/Controllers/MemberClaimsController.cs
+++ ClaimsArray/Controllers/MemberClaimsController.cs
@@ -3,29 +3,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClaimsArray.Services;
+using ClaimsArray.Mappers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ClaimsArray.Controllers
 {
-    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/MemberClaims")]
     [ApiController]
     public class MemberClaimsController : ControllerBase
     {
+        private const string claimsPath = @"../ClaimsArray/CSVs/Claim.csv";
+        private const string membersPath = @"../ClaimsArray/CSVs/Member.csv";
+
         // GET: api/<MemberClaimsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get(string claimDate)
         {
-            return new string[] { "value1", "value2" };
+            var claimRecords = new MemberClaimService<Claim, ClaimMap>();
+            var memberRecords = new MemberClaimService<Member, MemberMap>();
+            List<Claim> claims = claimRecords.ReadCSVFile(claimsPath);
+            List<Member> members = memberRecords.ReadCSVFile(membersPath);
+            var filteredClaimByClaimDate = claims.Where(x => x.ClaimDate == claimDate).ToList();
+            var claimArrayList = new List<ClaimArray>();
+            foreach(var filteredClaim in filteredClaimByClaimDate)
+            {
+                var claimArray = new ClaimArray();
+                var member = members.Where(x => x.ID == filteredClaim.MemberID).SingleOrDefault();
+                claimArray.Member = member;
+                claimArray.Claim = filteredClaim;
+                claimArrayList.Add(claimArray);
+            }
+            string claimsArray = JsonSerializer.Serialize(claimArrayList);
+            return claimsArray;
         }
 
-        // GET api/<MemberClaimsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
+      
         // POST api/<MemberClaimsController>
         [HttpPost]
         public void Post([FromBody] string value)
